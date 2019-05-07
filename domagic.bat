1e28cacaf10e7e7f9ba574eef008ab6301ca7328
Pablo Beltrán-Pellicer <pabelpe@gmail.com>
2019-05-07 19:16:13 +0200

    z

--- domagic.bat
+++ domagic.bat
@@ -1,1 +1,1 @@
-pandoc -t revealjs --from markdown+fenced_divs+fancy_lists+example_lists -s --mathjax main.md -o superestimacion.html -V revealjs-url=./reveal.js -V theme=my_black
+pandoc -t revealjs --from markdown+fenced_divs+fancy_lists+example_lists -s --mathjax index.md -o index.html -V revealjs-url=./reveal.js -V theme=my_black -V transition=cube
